ed31f7571c8a848df7361e626efbf10ba645ca91
Kishwar <kishwar.shafin@gmail.com>
2019-04-04 00:03:48 -0700

    dataloaders changed :fire:

--- modules/python/models/dataloader.py
+++ modules/python/models/dataloader.py
@@ -14,22 +14,28 @@ class SequenceDataset(Dataset):
 
     def __init__(self, hdf5_path, transform=None):
         self.transform = transforms.Compose([transforms.ToTensor()])
-        self.hdf5_file = h5py.File(hdf5_path, 'r')
-        self.summary_keys = list(self.hdf5_file['summaries'].keys())
+        self.hdf_filepath = hdf5_path
+        hdf5_file = h5py.File(hdf5_path, 'r')
+        self.total_summary_keys = len(list(hdf5_file['summaries'].keys()))
+        hdf5_file.close()
 
     def __getitem__(self, index):
         # load the image
-        hdf5_file = self.hdf5_file['summaries'][self.summary_keys[index]]
+        hdf5_file_ref = h5py.File(self.hdf_filepath, 'r')
+        key = list(hdf5_file_ref['summaries'].keys())[index]
+        hdf5_file = hdf5_file_ref['summaries'][key]
 
         image = hdf5_file['image']
         labels = hdf5_file['label']
+        # chromosome_name = hdf5_file['chromosome_name']
 
         image = torch.Tensor(image)
         # label = torch.(image).type(torch.DoubleStorage)
         label = np.array(labels, dtype=np.int)
         # label = torch.from_numpy(label).type(torch.LongStorage)
+        hdf5_file_ref.close()
 
         return image, label
 
     def __len__(self):
-        return len(self.summary_keys)
+        return self.total_summary_keys
