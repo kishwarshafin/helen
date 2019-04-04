2140678d98b62ee85ee9e13b0f95389fd6ab162b
Kishwar <kishwar.shafin@gmail.com>
2019-04-03 23:06:11 -0700

    a post-processing module added that accumulates all h5py files generated by marginpolish into one file :fire:

---
+++ modules/python/DataStore.py
@@ -0,0 +1,68 @@
+import h5py
+import yaml
+import numpy as np
+from modules.python.Options import ImageSizeOptions
+
+
+class DataStore(object):
+    """Class to read/write to a FRIDAY's file"""
+    _summary_path_ = 'summaries'
+    _groups_ = ('chromosome_name', 'image', 'position', 'index', 'label')
+
+    def __init__(self, filename, mode='r'):
+        self.filename = filename
+        self.mode = mode
+
+        self._sample_keys = set()
+        self.file_handler = None
+
+        self._meta = None
+
+    def __enter__(self):
+        self.file_handler = h5py.File(self.filename, self.mode)
+
+        return self
+
+    def __exit__(self, *args):
+        if self.mode != 'r' and self._meta is not None:
+            self._write_metadata(self.meta)
+        self.file_handler.close()
+
+    def _write_metadata(self, data):
+        """Save a data structure to file within a yml str."""
+        for group, d in data.items():
+            if group in self.file_handler:
+                del self.file_handler[group]
+            self.file_handler[group] = yaml.dump(d)
+
+    def _load_metadata(self, groups=None):
+        """Load meta data"""
+        if groups is None:
+            groups = self._groups_
+        return {g: yaml.load(self.file_handler[g][()]) for g in groups if g in self.file_handler}
+
+    @property
+    def meta(self):
+        if self._meta is None:
+            self._meta = self._load_metadata()
+        return self._meta
+
+    def update_meta(self, meta):
+        """Update metadata"""
+        self._meta = self.meta
+        self._meta.update(meta)
+
+    def write_train_summary(self, chromosome_name, image, position, index, label, summary_name):
+        # if 'summary' not in self.meta:
+        #     self.meta['summary'] = set()
+
+        # img_dset = self.file_handler.create_dataset('{}/{}/{}'.format(self._summary_path_, summary_name, 'image'),
+        #                                             (1000, 10), np.double)
+        # img_dset[...] = image
+        self.file_handler['{}/{}/{}'.format(self._summary_path_, summary_name, 'image')] = image
+        self.file_handler['{}/{}/{}'.format(self._summary_path_, summary_name, 'chromosome_name')] = chromosome_name
+        self.file_handler['{}/{}/{}'.format(self._summary_path_, summary_name, 'position')] = position
+        self.file_handler['{}/{}/{}'.format(self._summary_path_, summary_name, 'index')] = index
+        self.file_handler['{}/{}/{}'.format(self._summary_path_, summary_name, 'label')] = label
+
+
---
+++ post_process_hdf5.py
@@ -0,0 +1,196 @@
+import h5py
+import argparse
+import sys
+from tqdm import tqdm
+from os.path import isfile, join
+from os import listdir
+import concurrent.futures
+from modules.python.DataStore import DataStore
+
+
+def get_file_paths_from_directory(directory_path):
+    """
+    Returns all paths of files given a directory path
+    :param directory_path: Path to the directory
+    :return: A list of paths of files
+    """
+    file_paths = [join(directory_path, file) for file in listdir(directory_path) if isfile(join(directory_path, file)) and file[-2:] == 'h5']
+    return file_paths
+
+
+def chunks(file_names, threads):
+    """Yield successive n-sized chunks from l."""
+    chunks = []
+    for i in range(0, len(file_names), threads):
+        chunks.append(file_names[i:i + threads])
+    return chunks
+
+
+def file_reader_worker(file_names):
+    gathered_values = []
+    for hdf5_filename in file_names:
+        hdf5_file = h5py.File(hdf5_filename, 'r')
+        label_decoder = {'A': 1, 'C': 2, 'G': 3, 'T': 4, '_': 0}
+
+        chromosome_name = hdf5_filename.split('.')[-3].split("-")[0]
+        pos_start = int(hdf5_filename.split('.')[-3].split('-')[1])
+
+        image_dataset = hdf5_file['simpleWeight']
+        position_dataset = hdf5_file['position']
+
+        image = []
+        for image_line in image_dataset:
+            a_fwd, a_rev, c_fwd, c_rev, g_fwd, g_rev, t_fwd, t_rev, gap_fwd, gap_rev = list(image_line)
+            image.append([a_fwd, c_fwd, g_fwd, t_fwd, a_rev, c_rev, g_rev, t_rev, gap_fwd, gap_rev])
+
+        label = []
+        if 'label' in hdf5_file.keys():
+            label_dataset = hdf5_file['label']
+            for l in label_dataset:
+                label.append(label_decoder[chr(l[0])])
+
+        position = []
+        index = []
+        for pos, indx in position_dataset:
+            position.append(pos + pos_start)
+            index.append(indx)
+        gathered_values.append((chromosome_name, image, position, index, label, hdf5_filename.split('/')[-1]))
+    return gathered_values
+
+
+def write_to_file_parallel(input_files, hdf5_output_file_name, threads):
+    with DataStore(hdf5_output_file_name, 'w') as ds:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=threads) as executor:
+            file_chunks = chunks(input_files, max(64, int(len(input_files) / threads) + 1))
+            futures = [executor.submit(file_reader_worker, file_chunk) for file_chunk in file_chunks]
+            for fut in concurrent.futures.as_completed(futures):
+                if fut.exception() is None:
+                    gathered_values = fut.result()
+                    for chromosome_name, image, position, index, label, summary_name in gathered_values:
+                        ds.write_train_summary(chromosome_name, image, position, index, label, summary_name)
+                else:
+                    sys.stderr.write(fut.exception())
+                fut._result = None  # python issue 27144
+
+
+def process_marginpolish_h5py(marginpolish_output_directory, output_path, train_mode, threads):
+    all_hdf5_file_paths = sorted(get_file_paths_from_directory(marginpolish_output_directory))
+
+    if train_mode:
+        total_length = len(all_hdf5_file_paths)
+        selected_training_samples = int(total_length * 0.8)
+
+        training_samples = all_hdf5_file_paths[:selected_training_samples]
+        testing_samples = all_hdf5_file_paths[selected_training_samples:]
+
+        train_data_file_name = output_path + "train" + "_images_marginpolish" + ".hdf"
+        sys.stderr.write("WRITING " + str(len(training_samples)) + " SAMPLES TO train_images_marginpolish.hdf\n")
+
+        write_to_file_parallel(training_samples, train_data_file_name, threads)
+
+        test_data_file_name = output_path + "test" + "_images_marginpolish" + ".hdf"
+        sys.stderr.write("WRITING " + str(len(testing_samples)) + " SAMPLES TO test_images_marginpolish.hdf\n")
+
+        write_to_file_parallel(testing_samples, test_data_file_name, threads)
+    else:
+        train_data_file_name = output_path + "images_marginpolish" + ".hdf"
+        sys.stderr.write("WRITING " + str(len(all_hdf5_file_paths)) + " SAMPLES TO images_marginpolish.hdf\n")
+        write_to_file_parallel(all_hdf5_file_paths, train_data_file_name, threads)
+
+
+def write_to_file(all_hdf5_file_paths, hdf5_data_file):
+    for i in tqdm(range(0, len(all_hdf5_file_paths)), ncols=100):
+        hdf5_filename = all_hdf5_file_paths[i]
+        hdf5_file = h5py.File(all_hdf5_file_paths[i], 'r')
+        label_decoder = {'A': 1, 'C': 2, 'G': 3, 'T': 4, '_': 0}
+
+        chromosome_name = hdf5_filename.split('.')[-3].split("-")[0]
+        pos_start = int(hdf5_filename.split('.')[-3].split('-')[1])
+
+        image_dataset = hdf5_file['simpleWeight']
+        position_dataset = hdf5_file['position']
+
+        image = []
+        for image_line in image_dataset:
+            a_fwd, a_rev, c_fwd, c_rev, g_fwd, g_rev, t_fwd, t_rev, gap_fwd, gap_rev = list(image_line)
+            image.append([a_fwd, c_fwd, g_fwd, t_fwd, a_rev, c_rev, g_rev, t_rev, gap_fwd, gap_rev])
+
+        label = []
+        if 'label' in hdf5_file.keys():
+            label_dataset = hdf5_file['label']
+            for l in label_dataset:
+                label.append(label_decoder[chr(l[0])])
+
+        position = []
+        index = []
+        for pos, indx in position_dataset:
+            position.append(pos + pos_start)
+            index.append(indx)
+        hdf5_data_file.write_train_summary(chromosome_name, image, position, index, label, hdf5_filename.split('/')[-1])
+
+
+def read_marginpolish_h5py(marginpolish_output_directory, output_path, train_mode, threads):
+    all_hdf5_file_paths = sorted(get_file_paths_from_directory(marginpolish_output_directory))
+
+    if train_mode:
+        total_length = len(all_hdf5_file_paths)
+        selected_training_samples = int(total_length * 0.8)
+
+        training_samples = all_hdf5_file_paths[:selected_training_samples]
+        testing_samples = all_hdf5_file_paths[selected_training_samples:]
+
+        train_data_file_name = output_path + "train" + "_images_marginpolish" + ".hdf"
+        sys.stderr.write("WRITING " + str(len(training_samples)) + "SAMPLES TO train_images_marginpolish.hdf")
+
+        train_data_file = DataStore(train_data_file_name, mode='w')
+        train_data_file.__enter__()
+        write_to_file(training_samples, train_data_file)
+
+        test_data_file_name = output_path + "test" + "_images_marginpolish" + ".hdf"
+        sys.stderr.write("WRITING " + str(len(testing_samples)) + "SAMPLES TO test_images_marginpolish.hdf")
+
+        test_data_file = DataStore(test_data_file_name, mode='w')
+        test_data_file.__enter__()
+        write_to_file(testing_samples, test_data_file)
+    else:
+        train_data_file_name = output_path + "images_marginpolish" + ".hdf"
+
+        sys.stderr.write("WRITING " + str(len(all_hdf5_file_paths)) + "SAMPLES TO images_marginpolish.hdf")
+        train_data_file = DataStore(train_data_file_name, mode='w')
+        train_data_file.__enter__()
+        write_to_file(all_hdf5_file_paths, train_data_file)
+
+
+if __name__ == '__main__':
+    '''
+    Processes arguments and performs tasks.
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--marginpolish_h5py_dir",
+        type=str,
+        required=True,
+        help="H5PY file generated by HELEN."
+    )
+    parser.add_argument(
+        "--output_h5py_dir",
+        type=str,
+        required=True,
+        help="H5PY file generated by MEDAKA."
+    )
+    parser.add_argument(
+        "--threads",
+        type=int,
+        default=5,
+        help="Number of maximum threads for this region."
+    )
+    parser.add_argument(
+        "--train_mode",
+        type=lambda x: (str(x).lower() == 'true' or str(x).lower() == '1'),
+        default=False,
+        help="If true then compare labels too."
+    )
+
+    FLAGS, unparsed = parser.parse_known_args()
+    process_marginpolish_h5py(FLAGS.marginpolish_h5py_dir, FLAGS.output_h5py_dir, FLAGS.train_mode, FLAGS.threads)
+    # read_marginpolish_h5py(FLAGS.marginpolish_h5py_dir, FLAGS.output_h5py_dir, FLAGS.train_mode, FLAGS.threads)
