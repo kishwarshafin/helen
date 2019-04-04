2ad4f597144e012fc51061dfc61ef10c20e258d5
Kishwar <kishwar.shafin@gmail.com>
2019-04-03 23:52:45 -0700

    dataloader update :fire:

--- modules/python/models/dataloader_test.py
+++ modules/python/models/dataloader_test.py
@@ -13,46 +13,26 @@ class SequenceDataset(Dataset):
         A CSV file path
     """
 
-    def __init__(self, csv_path, transform=None):
-        data_frame = pd.read_csv(csv_path, header=None, dtype=str)
-        # assert data_frame[0].apply(lambda x: os.path.isfile(x.split(' ')[0])).all(), \
-        #     "Some images referenced in the CSV file were not found"
+    def __init__(self, hdf5_path, transform=None):
         self.transform = transforms.Compose([transforms.ToTensor()])
-        self.file_info = list(data_frame[0])
+        self.hdf5_file = h5py.File(hdf5_path, 'r')
+        self.summary_keys = list(self.hdf5_file['summaries'].keys())
 
     def __getitem__(self, index):
         # load the image
-        hdf5_filename = self.file_info[index]
-        label_decoder = {'A': 1, 'C': 2, 'G': 3, 'T': 4, '_': 0}
+        key = self.summary_keys[index]
+        hdf5_file = self.hdf5_file['summaries'][key]
 
-        hdf5_file = h5py.File(hdf5_filename, 'r')
-        chromosome_name = hdf5_filename.split('.')[-3].split('-')[0]
-
-        image_dataset = hdf5_file['simpleWeight']
-        label_dataset = hdf5_file['label']
-        # position_dataset = hdf5_file['position']
-
-        image = []
-        for image_line in image_dataset:
-            image.append(list(image_line))
-
-        labels = []
-        for label in label_dataset:
-            labels.append(label_decoder[chr(label[0])])
-
-        # position = []
-        # index = []
-        # for pos in position_dataset:
-        #     pos, indx = pos
-        #     position.append(pos)
-        #     index.append(indx)
-
-        hdf5_file.close()
+        image = hdf5_file['image']
+        labels = hdf5_file['label']
+        # chromosome_name = hdf5_file['chromosome_name']
 
         image = torch.Tensor(image)
+        # label = torch.(image).type(torch.DoubleStorage)
         label = np.array(labels, dtype=np.int)
+        # label = torch.from_numpy(label).type(torch.LongStorage)
 
-        return image, label, chromosome_name
+        return image, label
 
     def __len__(self):
-        return len(self.file_info)
+        return len(self.summary_keys)
